704ce8a173b786ddf0b52cc393e9d5dde5c7b90b
aabedelmauk <aabedelmauk>
2016-06-21 12:58:47 -0400

    bootstrap,css,resorces,javascriptcode

---
+++ JS/scripts.js
@@ -0,0 +1,64 @@
+
+//Javascript Review File
+
+console.log("Happy!");
+
+var age =16;
+var myBirthyear=2000;
+var graduation= 2018;
+var numberOfBrothersAndSisters =1;
+var numberofkids =2;
+
+//string concatentaion
+console.log("I am "  +  age + ".");
+console.log("I was born in " + myBirthyear +".");
+console.log("I graduated from Highschool in " + graduation +".");
+console.log(" I have  " +  numberOfBrothersAndSisters   +  " brothers and sisters");
+console.log(" I have " + numberofkids + " kids" );
+
+
+document.ElementById("my-age").innerHTML = age;
+
+
+document.getElementById("my-graduation").innerHTML = graduationyear;
+
+var abc ="abcdefghijklmnopqrstuvwxyz";
+var abcarray=["a","b","c"];
+var numberarray=[1,2,3,4,5];
+
+console.log(abc.charAt[3]);
+console.log(abcarrays[2]);
+
+
+//Another way to create and array
+
+
+var typesofsoda = new array();
+typesofsoda[0]= "red cream";
+typesofsoda[1]= "pepsi";
+typesofsoda[2]= "diet pepsi";
+typesofsoda[3]= "mountain Dew";
+
+
+var BestCandyBar=["snikers", "Twix","Hersheys","100 grand"];
+
+///Write a while loop that prints 0-20 by 2s
+
+
+for(var i =0; i <51; i +=5){
+	console.log(i);
+	if(i===50)
+	{
+		console.log("I am Rich");
+	}
+}
+
+
+
+
+/*
+var friends= ["joshua", "Jake", "Jerimia"];
+for(var i =0; i < friends.length; i++){
+	if(friends[i])
+}
+*/
